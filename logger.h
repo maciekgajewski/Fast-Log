0c6e8611868dec941b70dc7ce0d1d14bc321c348
Maciej Gajewski <maciejgajewski@Optiver.com>
2016-11-11 16:54:23 +0100

    compiles

--- logger.h
+++ logger.h
@@ -16,13 +16,31 @@ namespace Detail {
 using FormatFunction = void(*)(std::ostream& stream, const char* buffer);
 
 std::vector<char> gBuffer;
-	
+
+// helper
+template <typename... Types>
+constexpr unsigned sizeof_args(Types&&...) { return sizeof...(Types); }
+
+// helper
+constexpr size_t CountPlaceholders(const char* formatString)
+{
+	return (formatString[0] == '\0') ? 0 : (
+		(formatString[0] == '%' ? 1u : 0u) + CountPlaceholders(formatString + 1));
 }
 
+// write the given log line to a buffer
+template <typename... Args>
+void WriteLog(const char* formatString, const Args&... args)
+{
+	// TODO
 }
+
+} // ns detail
+
+} // ns Logger
 
 #define LOG(formatString, ...) \
-	static_assert(CountPlaceholders(formatString) == sizeof_args(__VA_ARGS__), "Number of arguments mismatch"); \
+	static_assert(Logger::Detail::CountPlaceholders(formatString) == Logger::Detail::sizeof_args(__VA_ARGS__), "Number of arguments mismatch"); \
 	do { \
 		Logger::Detail::WriteLog(formatString, ##__VA_ARGS__); \
 	} while (false);
--- main.cc
+++ main.cc
@@ -2,6 +2,6 @@
 
 int main(int argc, char* args[])
 {
-	LOG("Hello myInt=% myChar=% myDouble=%", 1, ‘a’, 42.3);
+	LOG("Hello myInt=% myChar=% myDouble=%", 1, 'a', 42.3);
 }
 
